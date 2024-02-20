58715122e93bccee2399a54877bd384115880721
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2024-02-20 16:43:28 +0300

    Add PlayerSettings

---
+++ Assets/Source/Scripts/Infrastructure/SaveLoadData/PlayerSettings.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Source.Scripts.Infrastructure.SaveLoadData
+{
+    [Serializable]
+    public class PlayerSettings
+    {
+        public bool IsSoundOn { get; private set; }
+        public bool IsMusicOn { get; private set; }
+        
+        public void SetSound(bool value) => 
+            IsSoundOn = value;
+
+        public void SetMusic(bool value) =>
+            IsMusicOn = value;
+    }
+}
