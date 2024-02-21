de271ce7fc12f759d5480edd988b1bd4b3cf0e7d
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2024-02-21 19:01:14 +0300

    Fix add capacity bag and implementation upgrade bag

--- Assets/Source/Scripts/SO/EnemyScriptableObject.cs
+++ Assets/Source/Scripts/SO/EnemyScriptableObject.cs
@@ -5,6 +5,7 @@ namespace Source.Scripts.SO
     [CreateAssetMenu(menuName = "Create ScriptableObject/Enemy", fileName = "Enemy", order = 1)]
     public class EnemyScriptableObject : ScriptableObject
     {
+        // TODO: переписать все SO с float на int !!!
         [field: SerializeField] public float Speed { get; private set; }
         [field: SerializeField] public int MaxHealth { get; private set; }
         [field: SerializeField] public int Damage { get; private set; }
